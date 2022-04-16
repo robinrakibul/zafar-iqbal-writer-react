73368a0ab97e50fad6f22c6bd00306b122426a8b
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-16 22:33:20 +0600

    nav items added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='shadow-md w-full fixed top-0 left-0'>
-            <div className='bg-white py-4'>
-                <div className='container px-4 cursor-pointer'>
-                    <h2 className='text-xl font-thin font-[poppins]'>Zafar Iqbal</h2>
+            <div className='bg-white py-5 px-5 flex font-[poppins] font-thin justify-between'>
+                <div className='container cursor-pointer'>
+                    <h2 className='text-xl'>
+                       Muhammed Zafar Iqbal</h2>
                 </div>
+                <ul>
+                    <Link className='pr-5' to="/home">Home</Link>
+                    <Link className='pr-5' to="/home#services">Services</Link>
+                    <Link className='pr-5' to="/home#experts">Experts</Link>
+                    <Link className='pr-5' to="/about">About</Link>
+                </ul>
             </div>
         </nav>
     );
