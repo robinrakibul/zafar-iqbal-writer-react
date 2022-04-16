1ec2a5eaa883a0c95f5484792ae88329e8a34ee1
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-16 23:15:51 +0600

    added simple responsiveness to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,13 @@ const Header = () => {
             <div className='bg-white py-5 px-5 flex font-[poppins] font-thin justify-between'>
                 <div className='container cursor-pointer'>
                     <h2 className='text-xl'>
-                       Muhammed Zafar Iqbal</h2>
+                    Zafar Iqbal</h2>
                 </div>
-                <ul className='flex'>
-                    <Link className='my-1 pr-5' to="/home">Home</Link>
-                    <Link className='my-1 pr-5' to="/home#services">Services</Link>
-                    <Link className='my-1 pr-5' to="/home#experts">Experts</Link>
-                    <Link className='my-1 pr-5' to="/about">About</Link>
+                <ul className='flex flex-wrap md:flex-nowrap'>
+                    <Link className='my-1 ml-2 md:ml-0 mb-2 md:mb-0 pr-5' to="/home">Home</Link>
+                    <Link className='my-1 ml-2 md:ml-0 md:mb-0 pr-5' to="/home#services">Services</Link>
+                    <Link className='my-1 ml-2 md:ml-0 md:mb-0 pr-5' to="/home#experts">Experts</Link>
+                    <Link className='my-1 ml-2 md:ml-0 mb-3 md:mb-0 pr-5' to="/about">About</Link>
                     <button className='break-words bg-blue-400 text-white font-[poppins] py-1 px-5 mr-5 rounded hover:bg-blue-800 duration-500'>Get&nbsp;Started</button>
                 </ul>
             </div>
