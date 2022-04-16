5125b5a3b90cd48d6b73af5f85d4eb37190e0740
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-16 21:41:43 +0600

    header component created

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+           <h2 className='text-3xl'>A Famous Writer</h2>
+        </div>
+    );
+};
+
+export default Header;
