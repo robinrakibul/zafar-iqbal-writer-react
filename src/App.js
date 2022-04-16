f7199259e9cb28f5d777e2ce80c57f31e0799d7c
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-17 00:07:08 +0600

    blank components created, routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
 
 function App() {
   return (
     <div className="App bg-indigo-200 w-full h-screen">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='shadow-md w-full fixed top-0 left-0'>
+        <header className='shadow-md w-full top-0 left-0'>
             <div className='bg-white py-5 px-5 flex font-[poppins] font-thin justify-between'>
                 <div className='container cursor-pointer'>
                     <span>
@@ -19,7 +19,7 @@ const Header = () => {
                     <button className='break-words bg-blue-400 text-white font-[poppins] py-1 px-5 mr-5 rounded hover:bg-blue-800 duration-500'>Get&nbsp;Started</button>
                 </ul>
             </div>
-        </nav>
+        </header>
     );
 };
 
