d130998df6ec13aa201ad20396f7eb95e7d38501
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-17 00:11:47 +0600

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+// Get Current Year
+const getYear = () => {
+    return new Date().getFullYear();
+}
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+       <footer className='fixed bottom-0 text-center w-full m-0 bg-blue-900 text-white p-5 font-[poppins]'>
+           <p><small>Copyright &copy; Rakibul Hassan Robin | {getYear()}</small></p>
+       </footer>
     );
 };
 
