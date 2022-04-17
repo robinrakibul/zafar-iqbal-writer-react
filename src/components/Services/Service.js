c2133394063aa61c3a8cb002782f07a20020cbde
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-17 22:32:33 +0600

    added responsiveness to homepage

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -5,15 +5,17 @@ const Service = ({ service }) => {
     const { title, price, description, image, category } = service;
     return (
         <div className='service'>
-            <div className='w-64 p-2'>
+            <div>
                 <h2 className='text-blue-900 font-sans pb-2'>Name: {title}</h2>
                 <img className='w-100 object-cover rounded border border-gray-400 shadow' src={image} alt="" />
                 <p><small><span className='text-blue-600'>Category: </span>{category}</small></p>
-                <p className='m-3'><small>{description.slice(0, 250)}...</small></p>
+                <div className='w-64'>
+                    <p className='m-3'><small>{description.slice(0, 250)}...</small></p>
+                </div>
                 <p className='p-2 font-bold font-mono text-xl'>Price: {price}</p>
-               <Link to="/checkout">
-               <button className='my-3 mb-10 text-xl font-mono font-medium py-1 px-4 bg-blue-600 rounded hover:bg-blue-800 text-white'>Buy Now</button>
-               </Link>
+                <Link to="/checkout">
+                    <button className='my-3 mb-10 text-xl font-mono font-medium py-1 px-4 bg-blue-600 rounded hover:bg-blue-800 text-white'>Buy Now</button>
+                </Link>
             </div>
         </div>
     );
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -11,7 +11,7 @@ const Services = () => {
     return (
         <div  id='services'>
             <div className="font-[poppins] font-bold text-3xl text-blue-600 text-center mt-10 mb-4">Services</div>
-            <div className="grid grid-cols-3 gap-3 px-10">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3 px-10">
             {
                 services.map(service => <Service key={service.id} service={service}></Service>)
             }
